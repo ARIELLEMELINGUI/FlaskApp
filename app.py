7ae4e7efd90f983a4199fc4a50ab26e6a7387370
arielle melingui <ariellemelingui18@gmail.com>
2026-03-24 10:04:28 +0100

    Fix: restauration du code fonctionnel

--- app.py
+++ app.py
@@ -2,16 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# --- CODE FONCTIONNEL (Mis en commentaire pour le test) ---
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-# ---------------------------------------------------------
-
-# --- ERREUR VOLONTAIRE POUR LE TP ---
-# On ajoute du texte n'importe où sans guillemets, 
-# ce qui va causer une "SyntaxError" lors de l'étape 'Run tests'
-Ceci est une erreur de syntaxe volontaire pour tester le blocage du pipeline !
+if __name__ == '__main__':
+    # On écoute sur toutes les interfaces (0.0.0.0) pour que Docker puisse acheminer le trafic
+    app.run(host='0.0.0.0', port=5000)
