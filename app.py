287537bc1d2fc968f78e8227b9b261e1d58834ed
arielle melingui <ariellemelingui18@gmail.com>
2026-03-24 09:53:51 +0100

    Test : introduction d'une erreur de syntaxe

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# --- CODE FONCTIONNEL (Mis en commentaire pour le test) ---
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+# ---------------------------------------------------------
+
+# --- ERREUR VOLONTAIRE POUR LE TP ---
+# On ajoute du texte n'importe où sans guillemets, 
+# ce qui va causer une "SyntaxError" lors de l'étape 'Run tests'
+Ceci est une erreur de syntaxe volontaire pour tester le blocage du pipeline !
